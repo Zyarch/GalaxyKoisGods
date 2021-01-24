91baf1da19e757c81d291a2933d0038b3e441982
Ian_LP <pillowi@etsu.edu>
2021-01-24 14:49:10 -0500

    Added basic class for gods, Amara is the only one in atm Added some test event to test god preferences and data access

--- src/main/java/com/Zyarch/GalaxyKoisGods/GalaxyKoisGods.java
+++ src/main/java/com/Zyarch/GalaxyKoisGods/GalaxyKoisGods.java
@@ -1,11 +1,18 @@
 package com.Zyarch.GalaxyKoisGods;
 
+import com.Zyarch.GalaxyKoisGods.data.GGod;
 import com.Zyarch.GalaxyKoisGods.setup.ModBlocks;
 import com.Zyarch.GalaxyKoisGods.setup.ModItems;
 import com.Zyarch.GalaxyKoisGods.setup.Registration;
 import net.minecraft.block.Block;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextComponent;
+import net.minecraftforge.common.ForgeConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,6 +21,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.awt.*;
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GalaxyKoisGods.MOD_ID)
 public class GalaxyKoisGods
@@ -21,6 +30,7 @@ public class GalaxyKoisGods
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "galasgods";
+    public static GGod godAmara;
 
     public GalaxyKoisGods() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -29,6 +39,10 @@ public class GalaxyKoisGods
         Registration.register();
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        godAmara = new GGod("Amara").
+                    setOfferItemList(Items.TOTEM_OF_UNDYING, Items.BONE_MEAL).
+                    setBadItemList(Items.WITHER_ROSE);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/com/Zyarch/GalaxyKoisGods/events/ModClientEvents.java
@@ -0,0 +1,58 @@
+package com.Zyarch.GalaxyKoisGods.events;
+
+import com.Zyarch.GalaxyKoisGods.GalaxyKoisGods;
+import com.Zyarch.GalaxyKoisGods.data.GGod;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.awt.event.ItemEvent;
+
+@Mod.EventBusSubscriber(modid = GalaxyKoisGods.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ModClientEvents {
+    @SubscribeEvent
+    public static void onDamageSpeakAmara(AttackEntityEvent event)
+    {
+        if (event.getTarget().isAlive()) {
+            PlayerEntity player = event.getPlayer();
+
+            if (!event.getPlayer().getEntityWorld().isRemote)
+            {
+                String msg = GalaxyKoisGods.godAmara.getName();
+                player.sendMessage(new StringTextComponent(msg), player.getUniqueID());
+            }
+        }
+    }
+    @SubscribeEvent
+    public static void onItemUse(ItemTossEvent event)
+    {
+        ItemEntity item = event.getEntityItem();
+        String itemName = item.getName().getString();
+        GGod _amara = GalaxyKoisGods.godAmara;
+        PlayerEntity player = event.getPlayer();
+        boolean isRemote = !event.getPlayer().getEntityWorld().isRemote;
+
+        if (_amara.isInOfferList(item.getItem()))
+        {
+            if (isRemote)
+            {
+                String msg = itemName + " is lovely!";
+                player.sendMessage(new StringTextComponent(msg), player.getUniqueID());
+            }
+        }
+        if (_amara.isInBadList(item.getItem()))
+        {
+            if (isRemote)
+            {
+                String msg = itemName + " is terrible!";
+                player.sendMessage(new StringTextComponent(msg), player.getUniqueID());
+            }
+        }
+    }
+}
