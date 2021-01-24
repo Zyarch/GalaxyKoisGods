170be2a3fe1ca4b4851b0323c457242193304a41
ragsdalep <43084059+Zyarch@users.noreply.github.com>
2021-01-23 19:25:02 -0500

    Data generators, tags
    Adding in data generators and tagging items. Need to add in the textures and the altar model. Maybe come back to this later today, but want to get out what I have.

---
+++ src/main/java/com/Zyarch/GalaxyKoisGods/data/ModItemTagsProvider.java
@@ -0,0 +1,30 @@
+package com.Zyarch.GalaxyKoisGods.data;
+
+import com.Zyarch.GalaxyKoisGods.GalaxyKoisGods;
+import com.Zyarch.GalaxyKoisGods.setup.ModItems;
+import com.Zyarch.GalaxyKoisGods.setup.ModTags;
+import net.minecraft.data.BlockTagsProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.ItemTagsProvider;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nullable;
+
+public class ModItemTagsProvider extends ItemTagsProvider {
+    public ModItemTagsProvider(DataGenerator dataGenerator, BlockTagsProvider blockTagProvider, ExistingFileHelper existingFileHelper) {
+        super(dataGenerator, blockTagProvider, GalaxyKoisGods.MOD_ID, existingFileHelper);
+    }
+
+    protected void registerTags() {
+        copy(ModTags.Blocks.ORES_SILVER, ModTags.Items.ORES_SILVER);
+        copy(Tags.Blocks.ORES, Tags.Items.ORES);
+
+        copy(ModTags.Blocks.STORAGE_BLOCKS_SILVER, ModTags.Items.STORAGE_BLOCKS_SILVER);
+        copy(Tags.Blocks.STORAGE_BLOCKS, Tags.Items.STORAGE_BLOCKS);
+
+        getOrCreateBuilder(ModTags.Items.INGOTS_SILVER).add(ModItems.SILVER_INGOT.get());
+        getOrCreateBuilder(Tags.Items.INGOTS).addTag(ModTags.Items.INGOTS_SILVER);
+    }
+}
---
+++ src/main/java/com/Zyarch/GalaxyKoisGods/data/client/ModBlockStateProvider.java
@@ -0,0 +1,21 @@
+package com.Zyarch.GalaxyKoisGods.data.client;
+
+
+import com.Zyarch.GalaxyKoisGods.GalaxyKoisGods;
+import com.Zyarch.GalaxyKoisGods.setup.ModBlocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockStateProvider  extends BlockStateProvider {
+    public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
+        super(gen, GalaxyKoisGods.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlock(ModBlocks.SILVER_BLOCK.get());
+        simpleBlock(ModBlocks.SILVER_ORE.get());
+        simpleBlock(ModBlocks.ALTAR.get());
+    }
+}
