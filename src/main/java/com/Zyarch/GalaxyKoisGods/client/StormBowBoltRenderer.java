b0107cd3192ca2d1434d8f3aae22edc4c1bf66de
Ian_LP <pillowi@etsu.edu>
2021-02-01 12:55:00 -0500

    Storm bow bolts have their own render and texture

--- src/main/java/com/Zyarch/GalaxyKoisGods/client/StormBowBoltRenderer.java
+++ src/main/java/com/Zyarch/GalaxyKoisGods/client/StormBowBoltRenderer.java
@@ -2,9 +2,13 @@ package com.Zyarch.GalaxyKoisGods.client;
 
 import com.Zyarch.GalaxyKoisGods.GalaxyKoisGods;
 import com.Zyarch.entity.projectiles.StormBowBoltEntity;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.entity.monster.BlazeEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -16,6 +20,13 @@ public class StormBowBoltRenderer extends ArrowRenderer<StormBowBoltEntity> {
         super(manager);
     }
 
+    public void render(StormBowBoltEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
+        super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, 15728880);
+    }
+
+    /*protected int getBlockLight(StormBowBoltRenderer entityIn, BlockPos partialTicks) {
+        return 15;
+    }*/
     /**
      * Returns the location of an entity's texture.
      */
