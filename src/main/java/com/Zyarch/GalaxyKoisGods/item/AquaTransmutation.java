d96a55b998b2163e88191182f50304769774edce
Ian_LP <pillowi@etsu.edu>
2021-05-05 23:28:09 -0400

    Implemented experimental code tu achieve swimming faster in watter Added Aqua Transmutation Item

---
+++ src/main/java/com/Zyarch/GalaxyKoisGods/item/AquaTransmutation.java
@@ -0,0 +1,31 @@
+package com.Zyarch.GalaxyKoisGods.item;
+
+import com.Zyarch.GalaxyKoisGods.GalaxyKoisGods;
+import com.Zyarch.GalaxyKoisGods.client.CultistRobeRenderer;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nullable;
+
+public class AquaTransmutation extends ArmorItem {
+    public AquaTransmutation(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+        super(materialIn, slot, builderIn);
+    }
+
+    @Nullable
+    @Override
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+        return (A) new CultistRobeRenderer(1.0f);
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return GalaxyKoisGods.MOD_ID + ":textures/model/armor/cult_robe.png";
+    }
+}
