3d3d0376e71cf3f93f687831330657f80d31f625
Ian <lukeip14cyan@gmail.com>
2023-12-03 03:03:10 -0600

    Update ClientEventBusSubscriber.java

--- src/main/java/com/zyarch/galaxykoisgods/client/ClientEventBusSubscriber.java
+++ src/main/java/com/zyarch/galaxykoisgods/client/ClientEventBusSubscriber.java
@@ -1,7 +1,12 @@
 package com.zyarch.galaxykoisgods.client;
 
 import com.zyarch.galaxykoisgods.GalaxyKoisGods;
+import com.zyarch.galaxykoisgods.setup.GalasEntityTypes;
+import com.zyarch.galaxykoisgods.setup.GalasItems;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -20,21 +25,25 @@ public class ClientEventBusSubscriber
         God.populateGodList();
 
         //Register item properties for Storm Bow animation
-        ItemModelsProperties.registerProperty(ModItems.STORM_BOW.get(), new ResourceLocation("pull"), (p_239429_0_, p_239429_1_, p_239429_2_) -> {
-            if (p_239429_2_ == null) {
-                return 0.0F;
-            } else {
-                return p_239429_2_.getActiveItemStack() != p_239429_0_ ? 0.0F : (float) (p_239429_0_.getUseDuration() - p_239429_2_.getItemInUseCount()) / 20.0F;
-            }
+        event.enqueueWork(() -> {
+            ItemProperties.register(GalasItems.STORM_BOW.get(), new ResourceLocation("pull"), (stack, level, living, id) -> {
+                if (living == null) {
+                    return 0.0F;
+                } else {
+                    return living.getUseItem() != stack ? 0.0F : (float) (stack.getUseDuration() - living.getUseItemRemainingTicks()) / 20.0F;
+                }
+            });
+            ItemProperties.register(GalasItems.STORM_BOW.get(), new ResourceLocation("pulling"),
+                    (stack, level, living, id) -> living != null && living.isUsingItem() && living.getUseItem() == stack ? 1.0F : 0.0F);
         });
-        ItemModelsProperties.registerProperty(ModItems.STORM_BOW.get(), new ResourceLocation("pulling"), (p_239428_0_, p_239428_1_, p_239428_2_) -> {
-            return p_239428_2_ != null && p_239428_2_.isHandActive() && p_239428_2_.getActiveItemStack() == p_239428_0_ ? 1.0F : 0.0F;
-        });
+    }
 
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         //register the Storm Bow Bolt Entity Type with the Storm Bow Renderer
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.STORM_BOW_BOLT.get(), StormBowBoltRenderer::new);
+        event.registerEntityRenderer(GalasEntityTypes.STORM_BOW_BOLT.get(), StormBowBoltRenderer::new);
 
         //register the Teleporting Orb Entity Type with the Teleporting Orb Renderer
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.TELEPORTING_ORB.get(), TeleportingOrbRenderer::new);
+        event.registerEntityRenderer(GalasEntityTypes.TELEPORTING_ORB.get(), TeleportingOrbRenderer::new);
     }
 }
